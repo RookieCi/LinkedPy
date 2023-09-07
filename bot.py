c6cbf5dbc1f9b55cfc05e959a750b9a91ecc788c
RookieCi <144260068+RookieCi@users.noreply.github.com>
2023-09-07 16:08:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,5 @@ import os
 import subproces
 import sys
 
-def sub():
-  subprocess.call(". /path/to/env.sh", shell = True)
-  subprocess.call("python something.py", shell = True)
+import os
+os.system('python script.py')
